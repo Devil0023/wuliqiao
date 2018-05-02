bc80e5d87bda05000d89f5f8cd25c9778e38cf93
Devil0023 <245092238@qq.com>
2018-05-02 23:58:51 +0800

    add exchange

---
+++ database/migrations/2018_05_02_155758_create_exchange_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExchangeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pid')->index()->comment('奖品id');
+            $table->integer('uid')->index()->comment('用户id');
+            $table->string('openid')->index()->comment('openid');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange');
+    }
+}
