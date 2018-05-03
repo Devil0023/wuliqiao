2c6ca146242805f4ce40629711194f7eef692d7a
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-03 13:42:15 +0800

    add wxuser routes

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,6 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
 
     $router->resource('/prize', PrizeController::class);
-    $router->resource('/wxuser', PrizeController::class);
+    $router->resource('/wxuser', WxuserController::class);
 
 });
