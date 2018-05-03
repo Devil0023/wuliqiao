0bde1384569f508e87da9931fe3b538e7f8f7516
ZHANGLEI-PC\Zhanglei <devil0023@gmail.com>
2018-05-03 22:16:39 +0800

    123

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,5 +16,5 @@ Route::group([
     $router->resource('/wxuser', WxuserController::class);
     $router->resource('/article', ArticleController::class);
 
-    $router->resource("/activity/{atype}", ActivityController::class);
+    $router->resource("/activity/{atype}/list", ActivityController::class);
 });
