24bc422be8d464bad8aa9de950ea7702da199ba1
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-04 15:07:40 +0800

    destroy test

--- app/Admin/Controllers/ActivityController.php
+++ app/Admin/Controllers/ActivityController.php
@@ -109,7 +109,7 @@ class ActivityController extends Controller
 
     public function destroy(Request $request){
 
-        if(Activity::destroy($request->info)){
+        if(Activity::destroy(explode(",", $request->info))){
             $result["status"]  = true;
             $result["message"] = "Delete succeeded !";
         }else{
