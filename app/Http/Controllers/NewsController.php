d811f6503af77d48333d248789c96a0eb453081a
ZHANGLEI-PC\Zhanglei <devil0023@gmail.com>
2018-05-06 20:42:32 +0800

    234

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,10 +10,13 @@ class NewsController extends Controller
 
     public function index(Request $request){
 
-        $page = intval($request->page);
+        $page = intval($request->page) == 0? 1: intval($request->page);
 
-        echo $page;
-        
+        //首页用view 翻页用json
+        if($page === 1){
+
+        }
+
         //要加缓存
         $list = Article::where("checked", 1)->where("deleted_at", null)
             ->orderBy("newstime", "desc")->paginate(1)->toJson();
