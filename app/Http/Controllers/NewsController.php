f9350df0959f675db6fcd6ac55d7fa2aa1d0040b
ZHANGLEI-PC\Zhanglei <devil0023@gmail.com>
2018-05-06 20:39:31 +0800

    123

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,11 +8,15 @@ use Illuminate\Http\Request;
 class NewsController extends Controller
 {
 
-    public function index(){
+    public function index(Request $request){
 
+        $page = intval($request->page);
+
+        echo $page;
+        
         //要加缓存
         $list = Article::where("checked", 1)->where("deleted_at", null)
-            ->orderBy("newstime", "desc")->paginate(1)->toJson(); //
+            ->orderBy("newstime", "desc")->paginate(1)->toJson();
 
         echo $list; die;
     }
