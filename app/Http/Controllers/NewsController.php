25e092ccc790a6b7f33a82e33736013a67680804
ZHANGLEI-PC\Zhanglei <devil0023@gmail.com>
2018-05-06 19:55:24 +0800

    123

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
     //
+
+    public function index(){
+
+        //要加缓存
+        $list = Article::where("checked", 1)->where("deleted_at", null)
+            ->orderBy("newstime", "desc")->paginate(10)->toJson();
+
+        echo $list; die;
+    }
 }
