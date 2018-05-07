9a2b2189755083df206ecac3d6b6099f8a1e4a4b
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-07 11:02:56 +0800

    234

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -12,15 +12,15 @@ class NewsController extends Controller
 
         $page = intval($request->page) == 0? 1: intval($request->page);
 
-        //首页用view 翻页用json
-        if($page === 1){
-
-        }
-
-        //要加缓存
         $list = Article::where("checked", 1)->where("deleted_at", null)
             ->orderBy("newstime", "desc")->paginate(1)->toJson();
 
-        echo $list; die;
+        //首页用view 翻页用json
+        if($page === 1){
+            return view();
+        }else{
+            return $list;
+        }
+
     }
 }
