a2f19b9e0fb4cc1e330c36820428ec3bb68bd6bc
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-07 16:52:10 +0800

    234

--- app/Models/Pointsrule.php
+++ app/Models/Pointsrule.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Pointslog;
+use App\Models\Ppointslog;
+use App\Models\Vpointslog;
 
 use DB;
 
