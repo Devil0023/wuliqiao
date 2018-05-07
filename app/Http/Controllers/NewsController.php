300a98a358ccea007ef074949f17cca800d077ca
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-07 13:18:46 +0800

    redis test

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -11,13 +11,25 @@ class NewsController extends Controller
     public function index(Request $request){
 
         $page = intval($request->page) == 0? 1: intval($request->page);
+        $size = 1;
 
-        $list = Article::where("checked", 1)->where("deleted_at", null)
-            ->orderBy("newstime", "desc")->paginate(1)->toJson();
+        $mkey = "Wuliqiao-Newslist-".$page."-".$size;
+        $list = @Redis::get($mkey);
+
+        if(empty($list)){
+            $list = Article::where("checked", 1)->where("deleted_at", null)
+                ->orderBy("newstime", "desc")->paginate(1)->toJson();
+            @Redis::setex($mkey, 300, $list);
+            echo 1;
+        }
+
+        echo 2;
+
+
 
         //首页用view 翻页用json
         if($page === 1){
-            return view();
+            return view("wechat.news", compact("list"));
         }else{
             return $list;
         }
