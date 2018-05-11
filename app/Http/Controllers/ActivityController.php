bb50b673e1f5174ebe65a100df841ebc869e29d7
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-11 13:57:31 +0800

    234

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use App\Models\Participate;
+use App\Models\Wxuser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
