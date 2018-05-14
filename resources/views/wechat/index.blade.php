63c7618fe13224da489fd03520572277d4ed1624
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-14 13:48:40 +0800

    index.blade

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,4 +1,13 @@
-{{csrf_field()}}
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title></title>
+</head>
+
+<body>
 
 {{$info["nickname"]}}
 {{$info["openid"]}}
@@ -8,3 +17,44 @@
 {{$info["volunteer_points"]}}
 {{$info["partymember"]}}
 {{$info["partymember_points"]}}
+
+<input type="button" class="sBtn" value="签到" id="sign" />
+
+<script src="http://skin.kankanews.com/v6/js/libs/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+    $("#enter").on("click",function(){
+
+        $.ajax({
+            cache: true,
+            type: "POST",
+            url:   "/wechat/usercenter/sign",
+            data:$("#jobN").serialize(),
+            async: false,
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+
+            error: function(data) {
+                console.log(data);
+            },
+
+            success: function(data) {
+
+                console.log(data);
+                //var dataObj = JSON.parse(data);
+
+//                if(data.error_code != "0"){
+//                    $(".mask").show();
+//                    $(".mask").find("p").html(data.error_message);
+//                }else{
+//                    $(".first").hide();
+//                    $(".second").show();
+//                };
+
+            }
+        });
+
+    });
+</script>
+
+</body>
