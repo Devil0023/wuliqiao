9dab242bb09e014a36e274486beafbe0fff1fa95
ZL-PC\Administrator <devil0023@gmail.com>
2018-05-14 15:29:57 +0800

    234

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -18,13 +18,13 @@ class NewsController extends Controller
     public function index(Request $request){
 
         $page = intval($request->page) == 0? 1: intval($request->page);
-        $size = 1;
+        $size = 5;
 
         $mkey = "Wuliqiao-Newslist-".$page."-".$size;
         $list = @Redis::get($mkey);
 
         if(empty($list)){
-            $list = Article::where("checked", 1)->orderBy("newstime", "desc")->paginate(1)->toJson();
+            $list = Article::where("checked", 1)->orderBy("newstime", "desc")->paginate($size)->toJson();
             @Redis::setex($mkey, 300, $list);
         }
 
@@ -32,8 +32,6 @@ class NewsController extends Controller
         $oauth        = session('wechat.oauth_user.default');
         $mission_key  = "Wuliqiao-Mission-".$oauth["id"];
         $mission_info = @Redis::get($mission_key);
-
-        echo $mission_info."||||||||||||";
 
         $mission["daily"]     = $this->dailymission;
         $mission["complete"] = count(array_filter(explode(",", $mission_info)));
--- resources/views/wechat/news.blade.php
+++ resources/views/wechat/news.blade.php
@@ -1,2 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title></title>
+</head>
+
+<body>
+
 任务：{{$mission["complete"]}}/{{$mission["daily"]}}
-{{json_encode($list)}}
+@foreach($list["data"] as $key => $val)
+
+    <a href="{{$val["url"]}}" id="read{{$val["id"]}}">{{$val["title"]}}</a><br/>
+
+@endforeach
+
+<script src="http://skin.kankanews.com/v6/js/libs/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+    $("#read1").on("click",function(){
+
+        $.ajax({
+            cache: true,
+            type: "POST",
+            url:   "/wechat/usercenter/sign",
+            data:  "newsid=",
+            async: false,
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+
+            error: function(data) {
+                console.log(data);
+            },
+
+            success: function(data) {
+
+                console.log(data);
+                //var dataObj = JSON.parse(data);
+
+//                if(data.error_code != "0"){
+//                    $(".mask").show();
+//                    $(".mask").find("p").html(data.error_message);
+//                }else{
+//                    $(".first").hide();
+//                    $(".second").show();
+//                };
+
+            }
+        });
+
+    });
+</script>
+
+</body>
